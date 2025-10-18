86e542aa7bbd9d86c0b5f6082f638e1d57832e11
Guerrouah Massinissa <massinissa.guerrouah@etu.u-pec.fr>
2025-10-18 19:56:40 +0200

    Fix routing HashRouter

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // racine du site
+  base: "/gogo-roadside-pro/",
   plugins: [react()],
   resolve: {
     alias: {
