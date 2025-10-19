1444332dd90bb1818dddc0dc7a17f8a85d487794
Guerrouah Massinissa <massinissa.guerrouah@etu.u-pec.fr>
2025-10-19 12:44:09 +0200

    Add Deno server for deployment

--- server.ts
+++ server.ts
@@ -1,4 +1,18 @@
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.190.0/http/file_server.ts";
 
-serve((req) => serveDir(req, { fsRoot: "./dist", showDirListing: true }));
+serve(async (req) => {
+  try {
+    const url = new URL(req.url);
+    const pathname = url.pathname === "/" ? "/index.html" : url.pathname;
+    return serveDir(req, {
+      fsRoot: "./dist",
+      urlRoot: "",
+      showDirListing: false,
+      indexFile: "index.html",
+      etag: true,
+    });
+  } catch {
+    return new Response("404 Not Found", { status: 404 });
+  }
+});
